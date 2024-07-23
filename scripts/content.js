9db7ac77f4b8601240057e252e5e57016fbc153f
kw0n0 <alwayskw0n0@gmail.com>
2024-07-23 19:39:03 +0900

    refactor: actions에 따라 해당 함수가 실행되도록 변경

--- scripts/content.js
+++ scripts/content.js
@@ -1,14 +1,23 @@
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.type == 'highlightColor') {
-    const style = document.querySelector('style[metamon]');
+const actions = {
+  highlightColor: changeHighlightColor,
+};
 
-    if (style) {
-      style.textContent = `::selection {background-color: ${request.data};}`;
-    } else {
-      const newStyle = document.createElement('style');
-      newStyle.textContent = `::selection {background-color: ${request.data};}`;
-      newStyle.setAttribute('metamon', true);
-      document.head.appendChild(newStyle);
-    }
+function changeHighlightColor(data) {
+
+  const style = document.querySelector('style[metamon]');
+
+  if (style) {
+    style.textContent = `::selection {background-color: ${data};}`;
+  } else {
+    const newStyle = document.createElement('style');
+    newStyle.textContent = `::selection {background-color: ${data};}`;
+    newStyle.setAttribute('metamon', true);
+    document.head.appendChild(newStyle);
+  }
+}
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (!actions[request.type]) return;
+
   }
 });
