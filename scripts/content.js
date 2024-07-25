3f6f6aae7efa179d734f834a5ce3f5a9a5df3735
kw0n0 <alwayskw0n0@gmail.com>
2024-07-26 01:35:37 +0900

    feat: modules dynamic import & async processing

--- scripts/content.js
+++ scripts/content.js
@@ -1,78 +1,30 @@
-//TODO: 모듈로 분리예정
-class Tooltip {
-  constructor({ width, height, id }) {
-    this.width = width;
-    this.height = height;
-    this.id = id;
-    this.initialize();
-  }
-
-  initialize() {
-    const box = document.createElement('div');
-    box.id = this.id;
-    box.style.position = 'absolute';
-    box.style.display = 'none';
-    //FIXME: 가장 높은 zIndex 설정방법 수정필요
-    box.style.zIndex = 99999;
-
-    const customStyle = {
-      width: `${this.width}px`,
-      height: `${this.height}px`,
-
-      backgroundColor: 'cyan',
-      borderRadius: '50px',
-    };
-
-    Object.assign(box.style, customStyle);
-    document.body.appendChild(box);
-  }
-
-  show({ top, left, right, bottom }) {
-    //FIXME: 위치 수정필요
-    const box = document.getElementById(this.id);
-    const posY = top < this.height ? bottom : top - this.height;
-    const posX = left < this.width ? right : left - this.width;
-
-    box.style.top = `${posY + window.scrollY}px`;
-    box.style.left = `${posX + window.scrollX}px`;
-    box.style.display = 'block';
-  }
+async function createTooltip() {
+  const result = await import('./modules/tooltip.js');
+  return result.Tooltip;
 }
 
-//TODO: 모듈로 분리예정
-const messages = {
-  highlightColor: changeHighlightColor,
-};
-
-function changeHighlightColor(data) {
-  const style = document.querySelector('style[metamon]');
-
-  if (style) {
-    style.textContent = `::selection {background-color: ${data};}`;
-  } else {
-    const newStyle = document.createElement('style');
-    newStyle.textContent = `::selection {background-color: ${data};}`;
-    newStyle.setAttribute('metamon', true);
-    document.head.appendChild(newStyle);
-  }
+async function createMessages() {
+  const result = await import('./modules/messages.js');
+  return result.messages;
 }
 
 class Content {
-  constructor({ messages }) {
-    this.messages = messages;
+  constructor({ createTooltip, createMessages }) {
+    this.createMessages = createMessages;
+    this.createTooltip = createTooltip;
   }
 
-  #onRuntimeMessage() {
+  #onRuntimeMessage({ messages }) {
     chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
       //MEMO: 메세지 수신 시 부가적인 작업이 많으면 분리예정
-      if (!this.messages[request.type]) return;
+      if (!messages[request.type]) return;
 
-      this.messages[request.type](request.data);
+      messages[request.type](request.data);
     });
   }
 
   //TODO: 이벤트리스너가 많아지는 경우에 대한 고려
-  #onSystemEvent() {
+  #onSystemEvent({ Tooltip }) {
     const tooltip = new Tooltip({
       width: 70,
       height: 30,
@@ -89,18 +41,21 @@ class Content {
         .getBoundingClientRect();
 
       tooltip.show({ top, right, left, bottom });
-      console.log(text);
     }
 
     document.body.addEventListener('mouseup', selectText);
   }
 
   listen() {
-    this.#onRuntimeMessage();
-    this.#onSystemEvent();
+    this.createMessages().then((messages) => {
+      this.#onRuntimeMessage({ messages });
+    });
+    this.createTooltip().then((Tooltip) => {
+      this.#onSystemEvent({ Tooltip });
+    });
   }
 }
 
-const content = new Content({ messages });
+const content = new Content({ createTooltip, createMessages });
 
 content.listen();
